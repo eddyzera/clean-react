8ec541918ec55cdd3251f2e8e9d1068b9c3f8f9c
eddyzera <edgar.nascimento@newgo.com>
2022-12-13 09:51:24 -0300

    feat: add authentication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { AccountModel } from 'domain/models/account-model'
+
+type AuthenticationParams = {
+  email: string
+  password: string
+}
+
+export interface Authentication {
+  auth (params: AuthenticationParams): Promise<AccountModel>
+}
--- src/hello.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('Test', () => {
-  it('aaa', () => {
-    expect(1).toBe(1)
-  })
-})
