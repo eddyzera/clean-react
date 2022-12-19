560998cdef0f55c22e12959123a0777b869dbac2
eddyzera <edgar.nascimento@newgo.com>
2022-12-19 16:29:14 -0300

    test: ensure AxiosHttpClient calls axios with correct URLs

--- src/infra/http/axiosHttpClient/axiosHttpClient.spec.ts
+++ src/infra/http/axiosHttpClient/axiosHttpClient.spec.ts
@@ -9,10 +9,10 @@ const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient()
 }
 describe('AxiosHttpClient', () => {
-  it('should call axios with correct URL', async () => {
+  it('should call axios with correct URL and verb', async () => {
     const url = faker.internet.url()
     const sut = makeSut()
     await sut.post({ url })
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
   })
 })
